93c74d1980217777020fd6584c377fad18aedff3
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-04-07 23:24:01 +0530

    removed unnecessary variable

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import MoviesCard from './MoviesCard';
 
 const API_URL = 'http://www.omdbapi.com/?apikey=e16fbf87';
 
-const movie = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-}
-
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
